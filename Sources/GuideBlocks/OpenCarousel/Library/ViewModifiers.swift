a1d445f1354fdc8ea0b6b35ae5c2ef754a9d2d1c
Aamir <alvi.aamir7@gmail.com>
2024-02-17 14:09:18 +0530

    Moved files from Folder iOS-GuidBlocks to Sources > GuidBlocks

--- Sources/GuideBlocks/OpenCarousel/Library/ViewModifiers.swift
+++ Sources/GuideBlocks/OpenCarousel/Library/ViewModifiers.swift
@@ -20,7 +20,7 @@ struct ContexualButton: View {
                     .frame(width: button.buttonSize.width, height: button.buttonSize.height, alignment: button.buttonTextAligment)
                     .background(Color(uiColor: button.backgroundColor ?? .black))
                     .foregroundStyle(Color(uiColor: button.textColor ?? .white))
-                    .contextualText(buttonElement: button)
+                    .contextualButtonFormat(button)
                     .clipShape(RoundedRectangle(cornerRadius: button.borderCornerRadius))
                     .border(Color(uiColor: button.borderColor ?? .clear), width: button.borderWidth)
                     .margin(button.margin)
